6844b4050ce1b24ec40f7c4e701efcdc27ad3c2c
siddmodi <siddharthmodi39101@gmail.com>
2022-06-17 15:20:05 +0530

    pymongo error solved

--- mongoDBOperations.py
+++ mongoDBOperations.py
@@ -12,7 +12,7 @@ class MongoDBManagement:
         try:
             self.username = username
             self.password = password
-            self.url = "mongodb+srv://{}:{}@cluster0.icufz.mongodb.net/?retryWrites=true&w=majority".format(
+            self.url = "mongodb+srv://{}:{}@cluster0.zpvnatn.mongodb.net/?retryWrites=true&w=majority".format(
                 self.username, self.password)
             # self.url = 'localhost:27017'
         except Exception as e:
